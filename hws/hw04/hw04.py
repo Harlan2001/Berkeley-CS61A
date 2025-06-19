182d622adfbd3547bbd2ca85f54f7b66d111b3d8
HARLAN XU <xuziliang9@gmail.com>
2025-06-19 17:59:35 +0800

    1

--- hws/hw04/hw04.py
+++ hws/hw04/hw04.py
@@ -1,91 +1,115 @@
+def deep_map(f, s):
+    """Replace all non-list elements x with f(x) in the nested list s.
+
+    >>> six = [1, 2, [3, [4], 5], 6]
+    >>> deep_map(lambda x: x * x, six)
+    >>> six
+    [1, 4, [9, [16], 25], 36]
+    >>> # Check that you're not making new lists
+    >>> s = [3, [1, [4, [1]]]]
+    >>> s1 = s[1]
+    >>> s2 = s1[1]
+    >>> s3 = s2[1]
+    >>> deep_map(lambda x: x + 1, s)
+    >>> s
+    [4, [2, [5, [2]]]]
+    >>> s1 is s[1]
+    True
+    >>> s2 is s1[1]
+    True
+    >>> s3 is s2[1]
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    for i in range(len(s)):
+        if isinstance(s[i], list):
+            deep_map(f, s[i])
+        else:
+            s[i] = f(s[i])
+
+
 HW_SOURCE_FILE=__file__
 
 
 def mobile(left, right):
     """Construct a mobile from a left arm and a right arm."""
-    assert is_arm(left), "left must be a arm"
-    assert is_arm(right), "right must be a arm"
+    assert is_arm(left), "left must be an arm"
+    assert is_arm(right), "right must be an arm"
     return ['mobile', left, right]
-
 
 def is_mobile(m):
     """Return whether m is a mobile."""
     return type(m) == list and len(m) == 3 and m[0] == 'mobile'
-
 
 def left(m):
     """Select the left arm of a mobile."""
     assert is_mobile(m), "must call left on a mobile"
     return m[1]
 
-#process Control Block PCB
-#
 def right(m):
     """Select the right arm of a mobile."""
     assert is_mobile(m), "must call right on a mobile"
     return m[2]
 
+
 def arm(length, mobile_or_planet):
-    """Construct a arm: a length of rod with a mobile or planet at the end."""
+    """Construct an arm: a length of rod with a mobile or planet at the end."""
     assert is_mobile(mobile_or_planet) or is_planet(mobile_or_planet)
     return ['arm', length, mobile_or_planet]
 
 def is_arm(s):
-    """Return whether s is a arm."""
+    """Return whether s is an arm."""
     return type(s) == list and len(s) == 3 and s[0] == 'arm'
 
 def length(s):
-    """Select the length of a arm."""
-    assert is_arm(s), "must call length on a arm"
+    """Select the length of an arm."""
+    assert is_arm(s), "must call length on an arm"
     return s[1]
 
 def end(s):
-    """Select the mobile or planet hanging at the end of a arm."""
-    assert is_arm(s), "must call end on a arm"
+    """Select the mobile or planet hanging at the end of an arm."""
+    assert is_arm(s), "must call end on an arm"
     return s[2]
 
-def planet(size):
-    """Construct a planet of some size."""
-    assert size > 0
+def planet(mass):
+    """Construct a planet of some mass."""
+    assert mass > 0
     "*** YOUR CODE HERE ***"
-    return ['planet', size]
 
-def size(w):
-    """Select the size of a planet."""
-    assert is_planet(w), 'must call size on a planet'
+def mass(p):
+    """Select the mass of a planet."""
+    assert is_planet(p), 'must call mass on a planet'
     "*** YOUR CODE HERE ***"
-    return w[1]
 
-def is_planet(w):
-    """Whether w is a planet."""
-    return type(w) == list and len(w) == 2 and w[0] == 'planet'
+def is_planet(p):
+    """Whether p is a planet."""
+    return type(p) == list and len(p) == 2 and p[0] == 'planet'
 
 def examples():
-    t = mobile(arm(1, planet(2)),arm(2, planet(1)))
-    u = mobile(arm(5, planet(1)),arm(1, mobile(arm(2, planet(3)),arm(3, planet(2)))))
+    t = mobile(arm(1, planet(2)),
+               arm(2, planet(1)))
+    u = mobile(arm(5, planet(1)),
+               arm(1, mobile(arm(2, planet(3)),
+                             arm(3, planet(2)))))
     v = mobile(arm(4, t), arm(2, u))
-    return (t, u, v)
+    return t, u, v
 
-def total_weight(m):
-    """Return the total weight of m, a planet or mobile.
+def total_mass(m):
+    """Return the total mass of m, a planet or mobile.
 
     >>> t, u, v = examples()
-    >>> total_weight(t)
+    >>> total_mass(t)
     3
-    >>> total_weight(u)
+    >>> total_mass(u)
     6
-    >>> total_weight(v)
+    >>> total_mass(v)
     9
-    >>> from construct_check import check
-    >>> # checking for abstraction barrier violations by banning indexing
-    >>> check(HW_SOURCE_FILE, 'total_weight', ['Index'])
-    True
     """
     if is_planet(m):
-        return size(m)
+        return mass(m)
     else:
-        assert is_mobile(m), "must get total weight of a mobile or a planet"
-        return total_weight(end(left(m))) + total_weight(end(right(m)))
+        assert is_mobile(m), "must get total mass of a mobile or a planet"
+        return total_mass(end(left(m))) + total_mass(end(right(m)))
 
 def balanced(m):
     """Return whether m is balanced.
@@ -95,12 +119,12 @@ def balanced(m):
     True
     >>> balanced(v)
     True
-    >>> w = mobile(arm(3, t), arm(2, u))
-    >>> balanced(w)
+    >>> p = mobile(arm(3, t), arm(2, u))
+    >>> balanced(p)
     False
-    >>> balanced(mobile(arm(1, v), arm(1, w)))
+    >>> balanced(mobile(arm(1, v), arm(1, p)))
     False
-    >>> balanced(mobile(arm(1, w), arm(1, v)))
+    >>> balanced(mobile(arm(1, p), arm(1, v)))
     False
     >>> from construct_check import check
     >>> # checking for abstraction barrier violations by banning indexing
@@ -108,240 +132,25 @@ def balanced(m):
     True
     """
     "*** YOUR CODE HERE ***"
-    if is_planet(m):
-        return True
-    l, r = end(left(m)), end(right(m))
-    return balanced(l) and balanced(r) and total_weight(l) * length(left(m)) == total_weight(r) * length(right(m))
 
-def totals_tree(m):
-    """Return a tree representing the mobile with its total weight at the root.
 
-    >>> t, u, v = examples()
-    >>> print_tree(totals_tree(t))
-    3
-      2
-      1
-    >>> print_tree(totals_tree(u))
-    6
-      1
-      5
-        3
-        2
-    >>> print_tree(totals_tree(v))
-    9
-      3
-        2
-        1
-      6
-        1
-        5
-          3
-          2
-    >>> from construct_check import check
-    >>> # checking for abstraction barrier violations by banning indexing
-    >>> check(HW_SOURCE_FILE, 'totals_tree', ['Index'])
-    True
+HW_SOURCE_FILE=__file__
+
+
+def max_path_sum(t):
+    """Return the maximum root-to-leaf path sum of a tree.
+    >>> t = tree(1, [tree(5, [tree(1), tree(3)]), tree(10)])
+    >>> max_path_sum(t) # 1, 10
+    11
+    >>> t2 = tree(5, [tree(4, [tree(1), tree(3)]), tree(2, [tree(10), tree(3)])])
+    >>> max_path_sum(t2) # 5, 2, 10
+    17
     """
     "*** YOUR CODE HERE ***"
-    if is_planet(m):
-        return tree(size(m))
-    else:
-        l, r = end(left(m)), end(right(m))
-        return tree(total_weight(m), [totals_tree(l), totals_tree(r)])
-
-
-def replace_leaf(t, find_value, replace_value):
-    """Returns a new tree where every leaf value equal to find_value has
-    been replaced with replace_value.
-
-    >>> yggdrasil = tree('odin',
-    ...                  [tree('balder',
-    ...                        [tree('thor'),
-    ...                         tree('freya')]),
-    ...                   tree('frigg',
-    ...                        [tree('thor')]),
-    ...                   tree('thor',
-    ...                        [tree('sif'),
-    ...                         tree('thor')]),
-    ...                   tree('thor')])
-    >>> laerad = copy_tree(yggdrasil) # copy yggdrasil for testing purposes
-    >>> print_tree(replace_leaf(yggdrasil, 'thor', 'freya'))
-    odin
-      balder
-        freya
-        freya
-      frigg
-        freya
-      thor
-        sif
-        freya
-      freya
-    >>> laerad == yggdrasil # Make sure original tree is unmodified
-    True
-    """
-    "*** YOUR CODE HERE ***"
-    if is_leaf(t):
-        return tree(replace_value if find_value == label(t) else label(t))
-    else:
-        return tree(label(t), [replace_leaf(b, find_value, replace_value) for b in branches(t)])
-
-
-def preorder(t):
-    """Return a list of the entries in this tree in the order that they
-    would be visited by a preorder traversal (see problem description).
-
-    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
-    >>> preorder(numbers)
-    [1, 2, 3, 4, 5, 6, 7]
-    >>> preorder(tree(2, [tree(4, [tree(6)])]))
-    [2, 4, 6]
-    """
-    "*** YOUR CODE HERE ***"
-    return [label(t)] + sum([preorder(b) for b in branches(t)],[])
-
-
-def has_path(t, phrase):
-    """Return whether there is a path in a tree where the entries along the path
-    spell out a particular phrase.
-
-    >>> greetings = tree('h', [tree('i'),
-    ...                        tree('e', [tree('l', [tree('l', [tree('o')])]),
-    ...                                   tree('y')])])
-    >>> print_tree(greetings)
-    h
-      i
-      e
-        l
-          l
-            o
-        y
-    >>> has_path(greetings, 'h')
-    True
-    >>> has_path(greetings, 'i')
-    False
-    >>> has_path(greetings, 'hi')
-    True
-    >>> has_path(greetings, 'hello')
-    True
-    >>> has_path(greetings, 'hey')
-    True
-    >>> has_path(greetings, 'bye')
-    False
-    """
-    assert len(phrase) > 0, 'no path for empty phrases.'
-    "*** YOUR CODE HERE ***"
-    if len(phrase) == 1:
-        return phrase[0] == label(t)
-    return label(t) == phrase[0] and any([has_path(b, phrase[1:]) for b in branches(t)])
 
 
 
-def interval(a, b):
-    """Construct an interval from a to b."""
-    return [a, b]
-
-def lower_bound(x):
-    """Return the lower bound of interval x."""
-    "*** YOUR CODE HERE ***"
-    return x[0]
-
-def upper_bound(x):
-    """Return the upper bound of interval x."""
-    "*** YOUR CODE HERE ***"
-    return x[1]
-def str_interval(x):
-    """Return a string representation of interval x.
-    """
-    return '{0} to {1}'.format(lower_bound(x), upper_bound(x))
-
-def add_interval(x, y):
-    """Return an interval that contains the sum of any value in interval x and
-    any value in interval y."""
-    lower = lower_bound(x) + lower_bound(y)
-    upper = upper_bound(x) + upper_bound(y)
-    return interval(lower, upper)
-
-def mul_interval(x, y):
-    """Return the interval that contains the product of any value in x and any
-    value in y."""
-    p1 = lower_bound(x) * lower_bound(y)
-    p2 = lower_bound(x) * upper_bound(y)
-    p3 = upper_bound(x) * lower_bound(y)
-    p4 = upper_bound(x) * upper_bound(y)
-    return interval(min(p1, p2, p3, p4), max(p1, p2, p3, p4))
-
-
-def sub_interval(x, y):
-    """Return the interval that contains the difference between any value in x
-    and any value in y."""
-    "*** YOUR CODE HERE ***"
-    return interval(lower_bound(x) - upper_bound(y), upper_bound(x) - lower_bound(y))
-
-
-def div_interval(x, y):
-    """Return the interval that contains the quotient of any value in x divided by
-    any value in y. Division is implemented as the multiplication of x by the
-    reciprocal of y."""
-    "*** YOUR CODE HERE ***"
-    assert(upper_bound(y) * lower_bound(y) > 0)
-    reciprocal_y = interval(1/upper_bound(y), 1/lower_bound(y))
-    return mul_interval(x, reciprocal_y)
-
-
-def multiple_references_explanation():
-    return """The multiple reference problem..."""
-
-
-
-def quadratic(x, a, b, c):
-    """Return the interval that is the range of the quadratic defined by
-    coefficients a, b, and c, for domain interval x.
-
-    >>> str_interval(quadratic(interval(0, 2), -2, 3, -1))
-    '-3 to 0.125'
-    >>> str_interval(quadratic(interval(1, 3), 2, -3, 1))
-    '0 to 10'
-    """
-    "*** YOUR CODE HERE ***"
-    def f(t):
-        return a*t*t + b*t +c
-    x1, x2 = lower_bound(x), upper_bound(x)
-    f1, f2 = f(x1), f(x2)
-    s = -b/(2*a)
-    minimum = f(s)
-    if x1 <= s <= x2:
-        if a > 0:
-            return interval(minimum, max(f1, f2))
-        else:
-            return interval(min(f1, f2), minimum)
-    else:
-        return interval(min(f1, f2), max(f1, f2))
-
-
-def par1(r1, r2):
-    return div_interval(mul_interval(r1, r2), add_interval(r1, r2))
-
-def par2(r1, r2):
-    one = interval(1, 1)
-    rep_r1 = div_interval(one, r1)
-    rep_r2 = div_interval(one, r2)
-    return div_interval(one, add_interval(rep_r1, rep_r2))
-def check_par():
-    """Return two intervals that give different results for parallel resistors.
-
-    >>> r1, r2 = check_par()
-    >>> x = par1(r1, r2)
-    >>> y = par2(r1, r2)
-    >>> lower_bound(x) != lower_bound(y) or upper_bound(x) != upper_bound(y)
-    True
-    """
-    r1 = interval(1, 100000) # Replace this line!
-    r2 = interval(1, 100000) # Replace this line!
-    return r1, r2
-
-
-
-# Tree ADT
+# Tree Data Abstraction
 
 def tree(label, branches=[]):
     """Construct a tree with the given label value and a list of branches."""
@@ -389,11 +198,12 @@ def print_tree(t, indent=0):
         4
         5
       6
+      25选秀
         7
     """
     print('  ' * indent + str(label(t)))
     for b in branches(t):
-        print_tree(b, indent + 1)
+        print_tree(b, indent + 1) 
 
 def copy_tree(t):
     """Returns a copy of t. Only for testing purposes.
@@ -405,4 +215,3 @@ def copy_tree(t):
     5
     """
     return tree(label(t), [copy_tree(b) for b in branches(t)])
-
